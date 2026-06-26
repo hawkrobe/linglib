1076ff29b98d1caf537119e32272e305921ecd49
Robert Hawkins <hawkrobe@gmail.com>
2026-06-26 15:45:20 -0700

    refactor(Syntax/Pronoun): graduate Pronoun WALS typology from Typology (#883)

--- Linglib.lean
+++ Linglib.lean
@@ -2881,6 +2881,7 @@ import Linglib.Syntax.Pronoun.Demonstrative
 import Linglib.Syntax.Pronoun.Indefinite
 import Linglib.Syntax.Pronoun.IndefiniteParadigm
 import Linglib.Syntax.Pronoun.Logophoric
+import Linglib.Syntax.Pronoun.WALS
 import Linglib.Syntax.Question
 import Linglib.Syntax.RelativeClause.Basic
 import Linglib.Syntax.RelativeClause.WALS
@@ -2910,5 +2911,5 @@ import Linglib.Typology.Negation.ExpletiveNegation
 
 import Linglib.Morphology.Number
 import Linglib.Typology.PolarityItem
-import Linglib.Typology.Pronoun.WALS
+
 import Linglib.Morphology.TenseAspect
