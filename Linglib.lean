482e6832f6fe2e7c2e58e9a21df85b896b275f23
Robert Hawkins <hawkrobe@gmail.com>
2026-08-05 15:51:16 -0700

    refactor(Syntax/Clause): merge Embedding into Context, rename enums

--- Linglib.lean
+++ Linglib.lean
@@ -2825,7 +2825,6 @@ import Linglib.Syntax.Clause.Basic
 import Linglib.Syntax.Clause.Chaining
 import Linglib.Syntax.Clause.Complementation
 import Linglib.Syntax.Clause.Context
-import Linglib.Syntax.Clause.Embedding
 import Linglib.Syntax.Clause.Frame
 import Linglib.Syntax.Comparative
 import Linglib.Syntax.ConstructionGrammar.ArgumentStructure
