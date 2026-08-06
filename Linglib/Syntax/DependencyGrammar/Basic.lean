a0ca07f8dc34bca642ba4c61d29a9f992060ef22
Robert Hawkins <hawkrobe@gmail.com>
2026-08-06 15:21:24 -0700

    docs(Syntax/DependencyGrammar): license headers on the seven substrate files (#1855)

--- Linglib/Syntax/DependencyGrammar/Basic.lean
+++ Linglib/Syntax/DependencyGrammar/Basic.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Robert Hawkins. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Robert Hawkins
+-/
 import Mathlib.Data.Fintype.Basic
 import Mathlib.Combinatorics.Digraph.Basic
 import Linglib.Data.UD.Basic
