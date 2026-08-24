7404afbf5f91f0a1c07a7e240ad35e0554b45a32
Robert Hawkins <hawkrobe@gmail.com>
2026-08-24 05:36:18 -0700

    chore: bump toolchain to v4.33.1, mathlib to v4.33.1

--- lakefile.lean
+++ lakefile.lean
@@ -18,7 +18,7 @@ require «doc-gen4» from git
 
 -- Mathlib last so its dependency versions take precedence
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "db584cd6d46c92f209a44c0f1c829460d327499d"
+  "https://github.com/leanprover-community/mathlib4" @ "0df444a360eaa60ab8c11dca51a86af692955474"
 
 @[default_target]
 lean_lib Linglib where
