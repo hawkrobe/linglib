d027cce420b82431b9e7999ab6fbc43dbc9fbab3
Robert Hawkins <hawkrobe@gmail.com>
2026-08-30 12:24:20 -0700

    refactor(Studies/BarwiseCooper1981): universals over GQ substrate (#2468)

--- Linglib.lean
+++ Linglib.lean
@@ -344,6 +344,7 @@ import Linglib.Data.Examples.BarAsherSiegal2026
 import Linglib.Data.Examples.BarLev2021
 import Linglib.Data.Examples.BarLevFox2020
 import Linglib.Data.Examples.Barker2002
+import Linglib.Data.Examples.BarwiseCooper1981
 import Linglib.Data.Examples.BeckOdaSugisaki2004
 import Linglib.Data.Examples.BeltramaSchwarz2024
 import Linglib.Data.Examples.BergenGoodman2015
