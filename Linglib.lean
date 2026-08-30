6d8638c16cfcb668c62a14974cce59c5a39fe7da
Robert Hawkins <hawkrobe@gmail.com>
2026-08-30 13:50:33 -0700

    refactor(Studies/Beaver2004): table (19) as a general theorem

--- Linglib.lean
+++ Linglib.lean
@@ -347,6 +347,7 @@ import Linglib.Data.Examples.BarLevFox2020
 import Linglib.Data.Examples.Barker2002
 import Linglib.Data.Examples.BarwiseCooper1981
 import Linglib.Data.Examples.Beaver2001
+import Linglib.Data.Examples.Beaver2004
 import Linglib.Data.Examples.BeaverCondoravdi2003
 import Linglib.Data.Examples.BeckOdaSugisaki2004
 import Linglib.Data.Examples.BeltramaSchwarz2024
