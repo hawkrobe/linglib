899f8f340de33bb0fa0d89d19537c01f77275db8
Robert Hawkins <hawkrobe@gmail.com>
2026-09-08 16:30:57 -0700

    refactor(Studies/Dendikken1995): particle placement from Case licensing (#2799)

--- Linglib.lean
+++ Linglib.lean
@@ -2124,8 +2124,7 @@ import Linglib.Studies.Dekker2012
 import Linglib.Studies.DelPinal2015
 import Linglib.Studies.DelPinalBassiSauerland2024
 import Linglib.Studies.DelPrete2013
-import Linglib.Studies.Dendikken1995ParticleVerbs
-import Linglib.Studies.Dendikken1995Resultatives
+import Linglib.Studies.Dendikken1995
 import Linglib.Studies.Denic2023
 import Linglib.Studies.DenicEtAl2021
 import Linglib.Studies.Deo2025
@@ -3034,3 +3033,4 @@ import Linglib.Data.Examples.DechaineWiltschko2002
 import Linglib.Data.Examples.Dekier2021
 import Linglib.Data.Examples.DelPrete2013
 import Linglib.Data.Examples.DeMarneffeNivre2019
+import Linglib.Data.Examples.Dendikken1995
