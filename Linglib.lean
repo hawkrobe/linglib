fcbb5949094013fbae54249995475768553a3a2b
Robert Hawkins <hawkrobe@gmail.com>
2026-09-09 12:04:01 -0700

    refactor(Studies/Funakoshi2016): null adjuncts from verb-stranding ellipsis (#2858)

--- Linglib.lean
+++ Linglib.lean
@@ -3068,3 +3068,4 @@ import Linglib.Data.Examples.FoxSpector2018
 import Linglib.Data.Examples.Francescotti1995
 import Linglib.Data.Examples.FrancikClark1985
 import Linglib.Data.Examples.FrischPierrehumbertBroe2004
+import Linglib.Data.Examples.Funakoshi2016
