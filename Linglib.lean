688d670b13ea4a424804c460aa192fcffccd3f38
Robert Hawkins <hawkrobe@gmail.com>
2026-09-12 03:54:26 -0700

    chore(Linglib): drop imports of renamed Krifka modules

--- Linglib.lean
+++ Linglib.lean
@@ -2358,8 +2358,6 @@ import Linglib.Studies.KratzerShimoyama2002
 import Linglib.Studies.Krejci2012
 import Linglib.Studies.Krifka1989
 import Linglib.Studies.Krifka1998
-import Linglib.Studies.Krifka2004
-import Linglib.Studies.Krifka2007
 import Linglib.Studies.Krifka2013
 import Linglib.Studies.Krifka2015
 import Linglib.Studies.Krifka2020
