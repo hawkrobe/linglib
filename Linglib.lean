31aa428c031b6f85db96b1bc4ef65196fa1e4249
Robert Hawkins <hawkrobe@gmail.com>
2026-09-17 10:03:58 -0700

    refactor(Core/Data/List): dissolve Config into an Int GetElem instance (#3789)

--- Linglib.lean
+++ Linglib.lean
@@ -101,7 +101,6 @@ import Linglib.Core.Data.Fintype.Transfer
 import Linglib.Core.Data.List.Chain
 import Linglib.Core.Data.List.Destutter
 import Linglib.Core.Data.List.EqOn
-import Linglib.Core.Data.List.Config
 import Linglib.Core.Data.List.DropRight
 import Linglib.Core.Data.List.Factors
 import Linglib.Core.Data.List.Perm
